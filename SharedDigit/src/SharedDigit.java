9d85fe7d0471584b8948ec6b1bd2f9f498cd48da
khangduymai <khangmai4591@gmail.com>
2018-10-04 01:10:34 -0700

    update

--- SharedDigit/src/SharedDigit.java
+++ SharedDigit/src/SharedDigit.java
@@ -1,27 +1,69 @@
 public class SharedDigit {
 
-    public  static void main(String[] args){
-        System.out.println(hasSharedDigit(10,23));
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(15, 65));
+
+        //13,33
+        //30,13
+        //15, 65
     }
 
-    public static boolean hasSharedDigit(int num1, int num2){
+    public static boolean hasSharedDigit(int num1, int num2) {
 
-        if((num1 < 10 || num1 > 99) || (num2 < 10 || num2 > 99)) {
+        if ((num1 < 10 || num1 > 99) || (num2 < 10 || num2 > 99)) {
             return false;
         }
 
-        boolean hasSharedDigit = true;
-
-        while(num1 > 0){
+        while (num1 > 0) {
             int lastDigitNum1 = num1 % 10;
 
-            while(num2 > 0){
+
+/*            for (int copyNum2 = num2; copyNum2 > 0; copyNum2 /= 10) {
+                int lastDigitNum2 = copyNum2 % 10;
+
+                if (lastDigitNum1 == lastDigitNum2) {
+                    return true;
+                }
+            }*/
+
+            int copyNum2 = num2;
+
+            while (copyNum2 > 0) {
+                int lastDigitNum2 = copyNum2 % 10;
+
+                if (lastDigitNum1 == lastDigitNum2) {
+                    return true;
+                }
+
+                copyNum2 /= 10;
+
+            }
+
+            num1 /= 10;
+
+        }
+
+
+        return false;
+    }
+}
+
+
+
+
+//Below is the wrong code which will have buggy
+
+/*        boolean hasSharedDigit = true;
+
+        while (num1 > 0) {
+            int lastDigitNum1 = num1 % 10;
+
+            while (num2 > 0) {
                 int lastDigitNum2 = num2 % 10;
 
-                if(lastDigitNum1 != lastDigitNum2){
+                if (lastDigitNum1 != lastDigitNum2) {
                     hasSharedDigit = false;
-                }
-                else{
+                } else {
                     hasSharedDigit = true;
                 }
 
@@ -34,5 +76,7 @@ public class SharedDigit {
         }
 
         return hasSharedDigit;
-    }
-}
+    }*/
+
+
+
